8440a681a737246008179641c31e1f2d1554e554
ryacub <ryacub@users.noreply.github.com>
2026-02-24 07:47:27 -0800

    chore: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 shortcutHelper.setFilePath("./shortcuts.xml")
 
-val appVersionCode = 132
+val appVersionCode = 133
 val lightNovelExpectedPluginApiVersion = 1
 
 android {
@@ -28,7 +28,7 @@ android {
         applicationId = "xyz.rayniyomi"
 
         versionCode = appVersionCode
-        versionName = "1.0.0"
+        versionName = "1.0.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
