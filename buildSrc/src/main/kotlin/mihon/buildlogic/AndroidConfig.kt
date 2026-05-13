629d33966b215cf85e349e07df22b1f0fe134181
ryacub <rayyacub@gmail.com>
2026-05-13 06:40:14 -0700

    chore: align native library dependencies for 16 KB pages (R642)
    Closes #471

--- buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
@@ -4,11 +4,11 @@ import org.gradle.api.JavaVersion as GradleJavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 object AndroidConfig {
-    const val COMPILE_SDK = 36
+    const val COMPILE_SDK = 37
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val NDK = "27.1.12297006"
-    const val BUILD_TOOLS = "36.0.0"
+    const val BUILD_TOOLS = "37.0.0"
 
     // https://youtrack.jetbrains.com/issue/KT-66995/JvmTarget-and-JavaVersion-compatibility-for-easier-JVM-version-setup
     val JavaVersion = GradleJavaVersion.VERSION_17
