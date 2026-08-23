56b9bfe5fbfa763a6184acf43bd95521db98c560
ryacub <ryacub@users.noreply.github.com>
2026-08-23 07:20:32 -0700

    R892: drop redundant self-package logcat import

--- core/common/src/main/java/tachiyomi/core/common/util/system/NonFatalReporter.kt
+++ core/common/src/main/java/tachiyomi/core/common/util/system/NonFatalReporter.kt
@@ -1,7 +1,6 @@
 package tachiyomi.core.common.util.system
 
 import logcat.LogPriority
-import tachiyomi.core.common.util.system.logcat
 import java.util.Collections
 import java.util.concurrent.ConcurrentHashMap
 
