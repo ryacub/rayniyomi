8997772068fee3c652dac5c352f758f655259fda
ryacub <ryacub@users.noreply.github.com>
2026-08-24 18:57:37 -0700

    R966: remove the invalid assert imports from the translation test

--- app/src/androidTest/java/eu/kanade/presentation/entries/manga/components/ChapterTranslationVisibilityAndroidTest.kt
+++ app/src/androidTest/java/eu/kanade/presentation/entries/manga/components/ChapterTranslationVisibilityAndroidTest.kt
@@ -3,8 +3,6 @@ package eu.kanade.presentation.entries.manga.components
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.test.assertDoesNotExist
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
