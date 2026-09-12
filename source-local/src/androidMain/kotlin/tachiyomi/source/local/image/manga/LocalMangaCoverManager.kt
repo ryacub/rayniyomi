73f1ef534e90dd6db4e3b0b372915f51c7d602f2
ryacub <ryacub@users.noreply.github.com>
2026-09-11 18:35:35 -0700

    R1059: close streams on local artwork file failures

--- source-local/src/androidMain/kotlin/tachiyomi/source/local/image/manga/LocalMangaCoverManager.kt
+++ source-local/src/androidMain/kotlin/tachiyomi/source/local/image/manga/LocalMangaCoverManager.kt
@@ -7,6 +7,7 @@ import eu.kanade.tachiyomi.util.storage.DiskUtil
 import tachiyomi.core.common.storage.nameWithoutExtension
 import tachiyomi.core.common.util.system.ImageUtil
 import tachiyomi.source.local.io.manga.LocalMangaSourceFileSystem
+import java.io.IOException
 import java.io.InputStream
 
 private const val DEFAULT_COVER_NAME = "cover.jpg"
@@ -34,12 +35,15 @@ actual class LocalMangaCoverManager(
             return null
         }
 
-        val targetFile = find(manga.url) ?: directory.createFile(DEFAULT_COVER_NAME)!!
-
-        inputStream.use { input ->
+        val targetFile = inputStream.use { input ->
+            val targetFile = find(manga.url) ?: directory.createFile(DEFAULT_COVER_NAME)
+            if (targetFile == null) {
+                throw IOException("Could not create the local manga cover file.")
+            }
             targetFile.openOutputStream().use { output ->
                 input.copyTo(output)
             }
+            targetFile
         }
 
         DiskUtil.createNoMediaFile(directory, context)
